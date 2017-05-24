8f433cf4399f5e77cbc95e9c17065e83fc9b37f0
Physixe <nathantonis@gmail.com>
2017-05-24 15:18:24 +0200

    Update Mirmillon.java

--- Mirmillon.java
+++ Mirmillon.java
@@ -28,13 +28,43 @@ public class Mirmillon extends Gladiateur {
      */
     Collection frappe;
 
-    public Mirmillon(String nom, Integer poids, Integer idg) {
+    public Mirmillon(String nom, Integer poids, Integer idg, Integer ide) {
+        super(nom,idg, ide);
+        this.poids = poids;
+    }
+    
+    public static void setC_poidsMax(Integer c_poidsMax) {
+        Mirmillon.c_poidsMax = c_poidsMax;
     }
 
-    public void recoitCoups(Gladiateur idg, Arme ida) {
+
+    public void recoitCoups(Gladiateur glad, Arme arme) {
+        frappe.add(glad);//se souvient du gladiateur qui l'a frappé
+        
+        Integer val_deff = 0;
+        for (Arme a : appartient){//accumule la valeur defensive des armes du gladiateur
+            val_deff += a.getValDef();
+        }
+        
+        Integer degats = glad.getForce() + arme.getValOff() - val_deff;//calcule la somme des degats et enleve la defense
+        
+        if (degats > 0){//empeche des dégats négatifs
+            this.setVie(this.getVie() - degats);
+        }
+        
     }
 
     public String rapporter() {
+        String rapport;
+        String armes = "";
+        
+        for (Arme a : appartient){
+            armes += a.getNom() + ", ";
+        }
+        
+        rapport = "Identifiant : " + this.getIdg() + " Nom : " + this.getNom() + " Ethnie : " + this.getIde() + " Etat : " + this.getEtat() + " Vie : " + this.getVie() + 
+        " Force : " + this.getForce() + " Poid : " + this.poids + " Armes : " + armes;
+        return rapport;
     }
 
     public Integer getForce() {
