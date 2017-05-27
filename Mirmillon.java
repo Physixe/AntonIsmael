468f22eebe463479b140cf8a4c2eb10b71c5a8b2
Physixe <nathantonis@gmail.com>
2017-05-27 17:46:58 +0200

    27/05
    Fin retiaire, continuer mirmillon

--- Mirmillon.java
+++ Mirmillon.java
@@ -7,64 +7,55 @@ public class Mirmillon extends Gladiateur {
      * @attribute
      */
     private static String c_type = "Mirmillon";
-
-    /**
-     * @attribute
-     */
     private static Integer c_poidsMax;
-
-    /**
-     * @attribute
-     */
     private Integer poids;
-
-    /**
-     * @associates <{uml.Arme}>
-     */
     static Collection armeAccessMirmillon;
-
-    /**
-     * @associates <{uml.Gladiateur}>
-     */
-    Collection frappe;
+    Collection<Gladiateur> agresseur;
 
     public Mirmillon(String nom, Integer poids, Integer idg, Integer ide) {
         super(nom,idg, ide);
         this.poids = poids;
     }
-    
+
     public static void setC_poidsMax(Integer c_poidsMax) {
         Mirmillon.c_poidsMax = c_poidsMax;
     }
 
 
     public void recoitCoups(Gladiateur glad, Arme arme) {
-        frappe.add(glad);//se souvient du gladiateur qui l'a frappé
-        
+        agresseur.add(glad);//se souvient du gladiateur qui l'a frappé
+
         Integer val_deff = 0;
         for (Arme a : appartient){//accumule la valeur defensive des armes du gladiateur
             val_deff += a.getValDef();
         }
-        
+
         Integer degats = glad.getForce() + arme.getValOff() - val_deff;//calcule la somme des degats et enleve la defense
-        
+
         if (degats > 0){//empeche des dégats négatifs
             this.setVie(this.getVie() - degats);
         }
-        
+
     }
 
     public String rapporter() {
-        String rapport;
-        String armes = "";
-        
-        for (Arme a : appartient){
-            armes += a.getNom() + ", ";
-        }
-        
-        rapport = "Identifiant : " + this.getIdg() + " Nom : " + this.getNom() + " Ethnie : " + this.getIde() + " Etat : " + this.getEtat() + " Vie : " + this.getVie() + 
-        " Force : " + this.getForce() + " Poid : " + this.poids + " Armes : " + armes;
-        return rapport;
+      String rapport;
+      String armes = "";
+
+      for (Arme a : appartient){
+          armes += a.getNom() + ", ";
+      }
+
+      rapport = "Identifiant : " + this.getIdg()
+      + "\nNom : " + this.getNom()
+      + "\nEthnie : " + getEthnie(this.getIde()).getNom();
+      + "\nEtat : " + this.getEtat()
+      + "\nVie : " + this.getVie()
+      + "\nForce : " + this.getForce()
+      + "\nPoids : " + this.poids()
+      + "\nArmes : " + armes;
+
+      return rapport;
     }
 
     public Integer getForce() {
--- Retiaire.java
+++ Retiaire.java
@@ -7,51 +7,68 @@ public class Retiaire extends Gladiateur {
      * @attribute
      */
     private static String c_type = "Retiaire";
-
-    /**
-     * @attribute
-     */
     private Integer agilite;
-
-    /**
-     * @attribute
-     */
     private static Integer c_force = 30;
-
-    /**
-     * @attribute
-     */
     private static Integer c_agiliteMax;
-
-    /**
-     * @associates <{uml.Arme}>
-     */
-    Collection armesAccessRetiaire;
+    Collection<Arme> armesAccessRetiaire;
 
     public Retiaire(String nom, Integer agilite, Integer idg) {
+      super(nom, ethnie);
+      this.agilite = agilite;
     }
 
     public static void c_setAgiliteMax(Integer a) {
+      c_agiliteMax = agilite;
     }
 
     public String rapporter() {
+      String rapport;
+      String armes = "";
+
+      for (Arme a : appartient){
+          armes += a.getNom() + ", ";
+      }
+
+      rapport = "Identifiant : " + this.getIdg()
+      + "\nNom : " + this.getNom()
+      + "\nEthnie : " + getEthnie(this.getIde()).getNom();
+      + "\nEtat : " + this.getEtat()
+      + "\nVie : " + this.getVie()
+      + "\nForce : " + this.getForce()
+      + "\nAgilité : " + this.getAgilite()
+      + "\nArmes : " + armes;
+
+      return rapport;
     }
 
-    public Collection getArmesDispoRet() {
+    public Collection<Arme> getArmesDispoRet() {
+      return armesAccessRetiaire;
     }
 
     public static void c_setForce(Integer f) {
+      this.force = f;
     }
 
-    public void recoitCoups(Gladiateur idg, Arme ida) {
+    public void recoitCoups(Gladiateur glad, Arme arme) {
+
+      Integer val_deff = 0;
+      for (Arme a : appartient){//accumule la valeur defensive des armes du gladiateur
+          val_deff += a.getValDef();
+      }
+
+      Integer degats = glad.getForce() + arme.getValOff() - val_deff;//calcule la somme des degats et enleve la defense
+
+      if (degats > 0){//empeche des dégats négatifs
+          this.setVie(this.getVie() - degats);
+      }
+
     }
 
     public Integer getForce() {
+      return this.force;
     }
 
-    public void addArmeDispoRet(Integer ida) {
-    }
-
-    public void autoriserArmeRetiaire() {
+    public void autoriserArmeRetiaire(Arme arme) {
+      this.armesAccessRetiaire.add(arme);
     }
 }
