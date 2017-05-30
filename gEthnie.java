8b8b71732a54a4ca6398199393eb5ee896af9c36
ismaelel <ismo67nhf@hotmail.fr>
2017-05-30 22:40:38 +0200

    Update gEthnie.java

--- gEthnie.java
+++ gEthnie.java
@@ -1,22 +1,63 @@
-package uml;
+package packglad;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 public class gEthnie {
     /**
      * @associates <{uml.Ethnie}>
      */
-    private Collection ttesLesEthnies;
+    private static ArrayList<Ethnie> ttesLesEthnies = new ArrayList<Ethnie>();
 
     public static Collection listerGladiateursDEthnie(Integer ide) {
+        int i=0;
+        Boolean fin = false;//booléen vrai si on a trouvé l'ethnie qu'on veut parmi celles de la collection
+        while (i< ttesLesEthnies.size() && !fin) 
+        {
+            if(ttesLesEthnies.get(i).getIde()==ide)
+            {
+                fin=true;
+            }
+            else
+                i++;
+        }
+        
+        return ttesLesEthnies.get(i).listerGladiateurs();
     }
 
-    public static Collection listerEthnie() {
+    public static ArrayList<Ethnie> listerEthnies() {
+        return ttesLesEthnies;
     }
 
     public static Ethnie getEthnie(Integer ide) {
+        int i=0;
+        Boolean fin = false;
+        while (i< ttesLesEthnies.size() && !fin) 
+        {
+            if(ttesLesEthnies.get(i).getIde()==ide)
+            {
+                fin=true;
+            }
+            else
+                i++;
+        }
+        
+        return ttesLesEthnies.get(i);
     }
 
     public static Integer getScore(Integer ide) {
+        int i=0;
+        Boolean fin = false;
+        while (i< ttesLesEthnies.size() && !fin) 
+        {
+            if(ttesLesEthnies.get(i).getIde()==ide)
+            {
+                fin=true;
+            }
+            else
+                i++;
+        }
+        
+        return ttesLesEthnies.get(i).calculerScore();
     }
 }
