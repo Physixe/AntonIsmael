072cdf07d25bedd0ebe22494baf8104e3df2a1c4
ismaelel <ismo67nhf@hotmail.fr>
2017-05-30 22:40:16 +0200

    Update Ethnie.java

--- Ethnie.java
+++ Ethnie.java
@@ -1,32 +1,36 @@
-package uml;
+package packglad;
 
-import java.util.ArrayList;
 import java.util.Collection;
+import java.util.ArrayList;
+
 
 public class Ethnie {
-    /**
-     * @attribute
-     */
     private Integer ide;
-
-    /**
-     * @attribute
-     */
     private String nom;
-    
+    private Integer score;
 
-    public Ethnie(Integer ide, String nom) {
+    private static ArrayList<Gladiateur> gladEthnie = new ArrayList<Gladiateur>();
+
+
+    public  Ethnie(Integer ide, String nom) {
+        this.ide=ide;
+        this.nom=nom;
+        this.score=0;
     }
 
     public Integer calculerScore() {
+        return this.score;
     }
 
     public Integer getIde() {
+        return this.ide;
     }
 
     public String getNom() {
+        return this.nom;
     }
 
-    public Collection<Gladiateur> listerGladiateurs() {
+    public ArrayList<Gladiateur> listerGladiateurs() {
+        return Ethnie.gladEthnie;
     }
 }
