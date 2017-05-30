0c166effc04bc3e14d3f4751f05f1b3bfd9092d2
ismaelel <ismo67nhf@hotmail.fr>
2017-05-30 22:41:36 +0200

    Update Retiaire.java

--- Retiaire.java
+++ Retiaire.java
@@ -1,75 +1,97 @@
-package uml;
+package packglad;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 public class Retiaire extends Gladiateur {
-    /**
-     * @attribute
-     */
+    
     private static String c_type = "Retiaire";
     private Integer agilite;
     private static Integer c_force = 30;
     private static Integer c_agiliteMax;
-    Collection<Arme> armesAccessRetiaire;
 
-    public Retiaire(String nom, Integer agilite, Integer idg) {
-      super(nom, ethnie);
-      this.agilite = agilite;
+    /**
+     * @associates <{uml.Arme}>
+     */
+    private static ArrayList<Arme> c_armesAccessRetiaire = new ArrayList<Arme>();
+
+    public Retiaire(String nom, Integer agilite, Ethnie ethnie) {
+        super(nom,ethnie);
+        this.agilite=agilite;
+    }
+    
+    public  Integer getAgilite(){
+        return agilite;
     }
 
     public static void c_setAgiliteMax(Integer a) {
         if (a>0)
-            c_agiliteMax = agilite;
+            c_agiliteMax=a;
     }
 
     public String rapporter() {
-      String rapport;
-      String armes = "";
+          String rapport;
+          String armes = "";
 
-      for (Arme a : appartient){
-          armes += a.getNom() + ", ";
-      }
+          for (Arme a : this.declarerArmes()){
+              armes += a.getNom() + ", ";
+          }
 
-      rapport = "Identifiant : " + this.getIdg()
-      + "\nNom : " + this.getNom()
-      + "\nEthnie : " + getEthnie(this.getIde()).getNom();
-      + "\nEtat : " + this.getEtat()
-      + "\nVie : " + this.getVie()
-      + "\nForce : " + this.getForce()
-      + "\nAgilité : " + this.getAgilite()
-      + "\nArmes : " + armes;
+          rapport = "Identifiant : " + this.getIdg()
+          + "\nNom : " + this.getNom()
+          + "\nEthnie : " + this.getEthnie().getNom()
+          + "\nEtat : " + this.getEtat()
+          + "\nVie : " + this.getVie()
+          + "\nForce : " + this.getForce()
+          + "\nAgilité : " + this.getAgilite()
+          + "\nArmes : " + armes;
 
-      return rapport;
-    }
+          return rapport;
+        }
 
-    public Collection<Arme> getArmesDispoRet() {
-      return armesAccessRetiaire;
+    public ArrayList<Arme> getArmesDispoRet() {
+        return c_armesAccessRetiaire;
     }
 
     public static void c_setForce(Integer f) {
-      this.force = f;
+        c_force=f;
     }
 
-    public void recoitCoups(Gladiateur glad, Arme arme) {
+    public void recoitCoups(Gladiateur glad, Arme arme) 
+    {
+        Integer val_deff = 0;
+        for (Arme a : this.declarerArmes()){//accumule la valeur defensive des armes du gladiateur
+            val_deff += a.getValDef();
+        }
+        
+        Integer degats = glad.getForce() + arme.getValOff() - val_deff;//calcule la somme des degats et enleve la defense
 
-      Integer val_deff = 0;
-      for (Arme a : appartient){//accumule la valeur defensive des armes du gladiateur
-          val_deff += a.getValDef();
-      }
-
-      Integer degats = glad.getForce() + arme.getValOff() - val_deff;//calcule la somme des degats et enleve la defense
-
-      if (degats > 0){//empeche des dégats négatifs
-          this.setVie(this.getVie() - degats);
-      }
+        if (degats > 0){//empeche des dégats négatifs
+            this.setVie(this.getVie() - degats);
+        }
 
     }
 
     public Integer getForce() {
-      return this.force;
+        return c_force;
     }
 
-    public void autoriserArmeRetiaire(Arme arme) {
-      this.armesAccessRetiaire.add(arme);
+
+    public static Integer c_autoriserArmeRetiaire(Arme arme) {
+        int res=-1;
+        if (gArme.getArmes().contains(arme) && !c_armesAccessRetiaire.contains(arme))
+        {
+            c_armesAccessRetiaire.add(arme);
+            res = arme.getIda();
+        }
+        return res;
+    }
+    
+    public String saluer(){
+        return "Ave Caesar, Rétiaire N°"+ this.getIdg()+ " : " + this.getNom() + ", j'appartiens à l'ethnie des " + this.getEthnie().getNom();
+    }
+    
+    public String getType(){
+        return "Retiaire";
     }
 }
